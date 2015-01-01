c127e275fdb7ddd4f0341a48367e1e0a49b42145
Vijay Ananth <vijay.hassan@gmail.com>
2015-01-02 00:46:08 +0530

    easy cases covered

--- roman_numeral_converter/roman_numeral_converter.rb
+++ roman_numeral_converter/roman_numeral_converter.rb
@@ -1,6 +1,14 @@
 class RomanNumeralConverter
   def convert(n)
-    true
+    result = ''
+     if (n >= 10)
+      n -= 10
+      result << "X"
+    end 
+    if (n >= 5)
+      n -= 5
+      result << "V"
+    end 
+    result <<  "I" * n
   end
-
 end
--- roman_numeral_converter/spec/roman_numeral_converter_spec.rb
+++ roman_numeral_converter/spec/roman_numeral_converter_spec.rb
@@ -5,5 +5,16 @@ RSpec::Given.use_natural_assertions
 describe RomanNumeralConverter do
   Given(:converter) {RomanNumeralConverter.new}
 
-  Then {converter.convert(1).should_not be_nil }
+  Then {converter.convert(1) == 'I' }
+  Then {converter.convert(2) == 'II' }
+  Then {converter.convert(3) == 'III' }
+  #Then {converter.convert(4) == 'IV' }
+  Then {converter.convert(5) == 'V' }
+  Then {converter.convert(6) == 'VI' }
+  Then {converter.convert(7) == 'VII' }
+  #Then {converter.convert(9) == 'IX' }
+  Then {converter.convert(10) == 'X' }
+  Then {converter.convert(11) == 'XI' }
+  Then {converter.convert(15) == 'XV' }
+  Then {converter.convert(16) == 'XVI' }
 end
